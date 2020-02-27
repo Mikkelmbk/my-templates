f4bc5059afafb3f7266b794289c1a50b24107dc7
Mikkel Back Koll <217236@RTS-365.dk>
2020-02-27 13:57:19 +0100

    kommenterede modal-slides-template

--- modal-slide-template/assets/js/script.js
+++ modal-slide-template/assets/js/script.js
@@ -6,24 +6,23 @@ let images = [
 	`/assets/img/monkaS.jpg`
 ];
 
-let modalImage = document.querySelector('.overlay-image');
 let galleryElem = document.querySelector('.gallery');
-let overlayImageIndex = 0;
+let overlayImageIndex = 0; // This is the number that will be used as "index" of the images array, Example: images[overlayImageIndex];
 
 images.forEach((image, index)=>{
 	let img = document.createElement('img');
-
+	
 	img.src = image;
 	img.classList.add('modal-images');
 	img.dataset.slideId = index;
-
+	
 	galleryElem.appendChild(img);
-
+	
 	img.addEventListener('click',(e)=>{
 		document.querySelector('#myModal').style.display = "flex";
-		overlayImageIndex = img.dataset.slideId;
+		overlayImageIndex = img.dataset.slideId; // Set overlayImageIndex to be the dataset that the image you clicked has.
 		handleOverlayImage()
-	
+		
 	})
 });
 
@@ -35,24 +34,25 @@ closeOverlayElem.addEventListener('click',()=>{
 
 let prev = document.querySelector('.prev');
 prev.addEventListener('click',()=>{
-	overlayImageIndex--;
-	if(overlayImageIndex < 0){
-		overlayImageIndex = images.length -1;
+	overlayImageIndex--; // subtract 1 from overlayImageIndex
+	if(overlayImageIndex < 0){ // If overlayImageIndex is less than 0
+		overlayImageIndex = images.length -1; // set overlayImageIndex to be the length of the images Array, minus 1.
 	}
 	handleOverlayImage()
 });
 
 let next = document.querySelector('.next');
 next.addEventListener('click',()=>{
-	overlayImageIndex++;
-	if(overlayImageIndex > images.length -1){
-		overlayImageIndex = 0;
+	overlayImageIndex++; // add 1 to overlayImageIndex
+	if(overlayImageIndex > images.length -1){ // If overlayImageIndex is greater than the length of the images array minus 1
+		overlayImageIndex = 0; // set overlayImageIndex to 0.
 	}
 	handleOverlayImage();
 });
 
 
+let modalImage = document.querySelector('.overlay-image');
 function handleOverlayImage(){
-	modalImage.src = images[overlayImageIndex];
+	modalImage.src = images[overlayImageIndex]; // Set the modalImage's src to be images[overlayImageIndex];
 }
 
