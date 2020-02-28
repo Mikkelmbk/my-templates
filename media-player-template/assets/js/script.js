6548c201935eac0980670678f91393fcf699aac2
Mikkel Back Koll <217236@RTS-365.dk>
2020-02-28 10:47:58 +0100

    video-player-template

--- media-player-template/assets/js/script.js
+++ media-player-template/assets/js/script.js
@@ -1,28 +1,85 @@
 let video = document.querySelector('.video-container video');
-
 let playPauseBtn = document.querySelector('.play-pause-btn');
+let videoTimeContainerElem = document.querySelector('.video-time-container');
+let displayTimeElem = document.querySelector('.display-time');
+let volumeControlElem = document.querySelector('.volume-input');
+let durationInputElem = document.querySelector('.duration-input');
+let time;
+let minutes;
+let seconds;
+let isHold = false;
 
-playPauseBtn.addEventListener('click',()=>{
+videoTimeContainerElem.style.width = `${video.offsetWidth}px`;
+
+playPauseBtn.classList.add('video-active');
+
+video.addEventListener('click',()=>{
 	if(video.paused){
 		video.play();
 		playPauseBtn.innerHTML = "&#10074;&#10074;";
-		playPauseBtn.style.opacity = 0;
+		videoTimeContainerElem.style.width = `${video.offsetWidth}px`;
 	}
 	else{
 		video.pause();
 		playPauseBtn.innerHTML = "&#9658;";
-		playPauseBtn.style.opacity = 1;
 	}
-
 })
 
 video.addEventListener('mouseenter',()=>{
 	if(!video.paused){
-		playPauseBtn.style.opacity = 1;
+		playPauseBtn.classList.add('video-active');
 	}
 })
+
 video.addEventListener('mouseleave',()=>{
 	if(!video.paused){
-		playPauseBtn.style.opacity = 0;
+		playPauseBtn.classList.remove('video-active');
 	}
 })
+
+video.addEventListener('timeupdate',(e)=>{
+	time = Math.floor(video.currentTime);
+	minutes = Math.floor(time / 60);
+	seconds = Math.floor(time - minutes * 60);
+
+	if(seconds < 10){
+		seconds = `0${seconds}`;
+	}
+
+	if(minutes < 10){
+		minutes = `0${minutes}`;
+	}
+
+	displayTimeElem.innerHTML = `${minutes}:${seconds}`;
+	
+	if(!isHold){
+		let currentDuration = (video.currentTime * 100) / video.duration;
+		durationInputElem.value = Math.floor(currentDuration);
+	}
+})
+
+volumeControlElem.addEventListener('input',()=>{
+	video.volume = (volumeControlElem.value / 100);
+});
+
+durationInputElem.addEventListener('mousedown',(e)=>{
+	isHold = true;
+});
+
+durationInputElem.addEventListener('mouseup',()=>{
+	isHold = false;
+})
+
+durationInputElem.addEventListener('input',(e)=>{
+	video.currentTime = (video.duration * e.target.value) / 100;
+})
+
+
+
+
+
+
+
+
+
+
