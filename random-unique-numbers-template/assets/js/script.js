47fe407e614951f891e9b9a302f84e8ef2a51c37
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-03-18 16:03:36 +0100

    kiggede mere på Set objektet og rettede templaten så den giver mere mening og viser hvordan man nemt kan bruge Spread operatoren til at lave en Set Collection om til et normalt Array

--- random-unique-numbers-template/assets/js/script.js
+++ random-unique-numbers-template/assets/js/script.js
@@ -1,9 +1,14 @@
-var numbers = new Set(); // Set acceptere KUN unikke værdier.
+var numbers = new Set(); // Set acceptere KUN unikke værdier. Set er IKKE et array, men en Collection af Items.
+
 
 while(numbers.size<10){ // .size er noget man kan bruge på Set objekter, den kigger på størrelsen af objektet.
     numbers.add(1+Math.floor(Math.random() * 100)); // .add tilføjer en værdi til Set Objektet, men KUN hvis den værdi ikke allerede findes, derfor kan forløkken her kører mere end 10 gange, da den ikke kigger på antal værdier generetet, men derimod antal UNIKKE værdier til stede i Set objektet.
 }
-numbers.forEach((number)=>{ // forEacher på Set Objektet.
+
+numbers = [...numbers]; // konvertere numbers Set til et normalt array med SPREAD operatoren [...DET-DU-VIL-GØRE-TIL-ET-ARRAY];
+
+console.log('numbers: ', numbers);
+numbers.forEach((number)=>{ // forEacher Set objektet som nu er et Array grundet spread operatoren ovenover.
 console.log('number: ', number);
 
 })
