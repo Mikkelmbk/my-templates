32095d7c5eeee541037dabf1e3ff517622376983
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-03-20 16:41:25 +0100

    OOP-class-polymorphism-template

---
+++ OOP-class-polymorphism-template/assets/js/script.js
@@ -0,0 +1,27 @@
+class Animal {
+    constructor(_name){
+        this.name = _name;
+    }
+
+    makeSound(){ // declare function in Animal class, make it say Generic animal sound!.
+        console.log("Generic animal sound!");
+    }
+}
+
+
+class Dog extends Animal {
+    constructor(_name){
+        super(_name);
+    }
+
+    makeSound(){ // overwrite function within Dog class inherited from Animal, make it say Woof woof!. (This is polymorphism)
+        console.log("Woof woof!");
+    }
+}
+
+
+let a1 = new Animal("Mikkel");
+let a2 = new Dog("Kenneth");
+
+a1.makeSound(); // call makeSound on a1 (Animal class);
+a2.makeSound(); // class makeSound on a2 (Dog class);
