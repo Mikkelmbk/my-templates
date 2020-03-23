d8dc1cf119ed19798ea62495c7dd901a66b4c2d0
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-03-23 14:17:17 +0100

    node-export-require-template-updated-v2

--- node-export-require-template/multi-export.js
+++ node-export-require-template/multi-export.js
@@ -8,7 +8,7 @@ function subtractNumbers(a, b){
 }
 
 
-function outputResult(result){
+function outputResult(result){ // this function is not exported, so it cannot be called in other files, however calling addNumbers or subtractNumbers will utilize this function, thus making it a "Helping" function that is only useable through the exported functions.
     console.log(result);
 }
 
--- node-export-require-template/single-export.js
+++ node-export-require-template/single-export.js
@@ -2,7 +2,7 @@ function printMessage(msg){
     handleMessage(`My name is ${msg}`);
 }
 
-function handleMessage(msg){
+function handleMessage(msg){// this function is not exported, so it cannot be called in other files, however calling addNumbers or subtractNumbers will utilize this function, thus making it a "Helping" function that is only useable through the exported functions.
     console.log(`${msg} and I am 25 years of age.`);
 }
 
