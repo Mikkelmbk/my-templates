ab5b6d4fadeef6981d2a33e40a6c8d9701ca9827
Mikkel <Mikkelmbk@users.noreply.github.com>
2021-07-25 11:01:21 +0200

    renamed and restructured mutationObserver example

--- mutationObserver-template/attribute-mutations/assets/js/script.js
+++ mutationObserver-template/attribute-mutations/assets/js/script.js
@@ -3,74 +3,49 @@ if('MutationObserver' in window){
     var customerElementToListenOn = "."+"title"; // This is a reference to the element that we want to listen to changes on.
     var customerElementChangeToListenFor = "Something" // This is the id or class that we want to listen for either being added or removed to the "customerElementToListenOn".
     var titleElement = document.querySelector(customerElementToListenOn);
-    var selectorState = {
-        classState: false,
-        idState: false,
-    }
 
-
-    var config = { attributes:true, childList: true, subtree:true };
+    var config = { attributes:true, attributeOldValue:true };
     var watchCustomerElement = function(mutationsList, observer){
-
     // Mutations are an array, so we loop through it.
         mutationsList.forEach(function(mutation){
-            console.log(mutation);
             if(mutation.type === 'attributes'){
 
                 if(mutation.target === document.querySelector(customerElementToListenOn)){
 
                     if(classActiveChecker(mutation)){ // This is true if the specified class was added to the "customerElementToListenOn".
-                        selectorState.classState = true;
-                        console.log(`The class "${customerElementChangeToListenFor}" was added to the element ${titleElement}`);
-                        // console.log(mutation.target.className);
-                        // document.querySelector("body").style.backgroundColor = "black";
 
                     }
                     else if(classInactiveChecker(mutation)){ // This is true if the specified class was removed from "customerElementToListenOn".
-                        selectorState.classState = false;
-                        console.log(`The class "${customerElementChangeToListenFor}" was removed from the element ${titleElement}`);
-                        // document.querySelector("body").style.backgroundColor = "white";
-
 
                     }
                     else if(idActiveChecker(mutation)){ // This is true if the specified id was added to the "customerElementToListenOn".
-                        selectorState.idState = true;
-                        console.log(`The Id "${customerElementChangeToListenFor}" was added to the element ${titleElement}`);
 
                     }
                     else if(idInactiveChecker(mutation)){ // This is true if the specified id was removed from "customerElementToListenOn".
-                        selectorState.idState = false;
-                        console.log(`The Id "${customerElementChangeToListenFor}" was removed from the element ${titleElement}`);
 
                     }
                     
                 }
 
             }
-            else if(mutation.type === 'childList'){
-
-            }
-            else if(mutation.type === 'subtree'){
-
-            }
         })
 
     };
-
     const observer = new MutationObserver(watchCustomerElement);
     observer.observe(titleElement,config);
 
+    // console.log(observer);
     function classActiveChecker(mutation){
-        if(mutation.attributeName === "class" && !selectorState.classState) return mutation.target.classList.contains(customerElementChangeToListenFor);
+        if(mutation.attributeName === "class" && mutation.target.className !== mutation.oldValue) return mutation.target.classList.contains(customerElementChangeToListenFor);
     }
     function classInactiveChecker(mutation){
-        if(mutation.attributeName === "class" && selectorState.classState) return !mutation.target.classList.contains(customerElementChangeToListenFor);
+        if(mutation.attributeName === "class" && mutation.target.className !== mutation.oldValue) return !mutation.target.classList.contains(customerElementChangeToListenFor);
     }
     function idActiveChecker(mutation){
-        if(mutation.attributeName === "id" && !selectorState.idState) return mutation.target.id === customerElementChangeToListenFor;
+        if(mutation.attributeName === "id" && mutation.target.id !== mutation.oldValue) return mutation.target.id === customerElementChangeToListenFor;
     }
     function idInactiveChecker(mutation){
-        if(mutation.attributeName === "id" && selectorState.idState) return mutation.target.id !== customerElementChangeToListenFor;
+        if(mutation.attributeName === "id" && mutation.target.id !== mutation.oldValue) return mutation.target.id !== customerElementChangeToListenFor;
     }
 
 
