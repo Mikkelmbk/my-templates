40478aa92272e3e602861e9a3147e7516e383034
Mikkel <mikkelmbk@live.dk>
2021-08-08 12:37:42 +0200

    addMutationObserver_v3

--- mutationObserver-template/addMutationListener-template/assets/js/script.js
+++ mutationObserver-template/addMutationListener-template/assets/js/script.js
@@ -1,6 +1,8 @@
 if ('MutationObserver' in window) {
   
-    addMutationListener('.outer-wrapper > .inner-wrapper > .container', "attributes_subtree", yourFunction);
+    addMutationListener('#unique-wrapper > .inner-wrapper > .container', "attributes_subtree", yourFunction);
+    //comments
+    {
     // Parameter 1: element (class or Id) you want to listen to selector.
     // Parameter 2: mutation you want to listen for - possible settings (childList OR attributes OR childList_subtree OR attributes_subtree).
     // Parameter 3: a reference to your function, no invocation, just referencing - you will receive the mutation/event as a parameter upon invocation by the mutationObserver.
@@ -22,7 +24,7 @@ if ('MutationObserver' in window) {
     // Use cases:
     // The addMutationListener can be used to update cart recom titles when a page refresh is not triggered upon changing or updating the cart content.
     // The addMutationListener can be used to trigger an upsell recom.
-
+    }
 }
 
 
@@ -34,12 +36,6 @@ function addMutationListener(element, mutationType, action) {
     if(!action || typeof(action) !== 'function' || action == "" || action == "undefined") return;
 
     let mutationConfig;
-    let mutationElement = document.querySelectorAll(element);
-    if(mutationElement.length > 1){
-        console.error("There are " + mutationElement.length + " elements with " + JSON.stringify(element) + " as their selector, we are listening to the first one we found");
-    }
-    mutationElement = mutationElement[0];
-
     if(mutationType.toLowerCase() === 'attributes'){
         mutationConfig = { attributes:true, attributeOldValue:true };
     }
@@ -53,33 +49,28 @@ function addMutationListener(element, mutationType, action) {
         mutationConfig = { childList:true, subtree:true };
     }
     else{
-        return console.error(mutationType + " is not a valid mutation to listen for, refer to Parameter 2 documentation for options.");
+        return console.error("addMutationListener: " + mutationType + " is not a valid mutation to listen for, refer to Parameter 2 documentation for options.");
     }
-    
 
+    let mutationElement = document.querySelectorAll(element);
+    if(mutationElement.length > 1){
+        console.warn("addMutationListener: There are " + mutationElement.length + " elements with " + JSON.stringify(element) + " as their selector, listening to the first one that was found");
+    }
+    mutationElement = mutationElement[0];
     let observer = new MutationObserver(function (mutationsList, observer) {
         mutationsList.forEach(function(mutation){
-            if (mutation.type === 'childList' && mutationType.toLowerCase() === 'childlist') {
-                action(mutation);
-            }
-            if(mutation.type === 'attributes' && mutationType.toLowerCase() === 'attributes'){
-                action(mutation);
-            }
-            if(mutation.type === 'childList' && mutationType.toLowerCase() === 'childlist_subtree'){
-                action(mutation);
-            }
-            if(mutation.type === 'attributes' && mutationType.toLowerCase() === 'attributes_subtree'){
-                action(mutation);
-            }
+            if(mutation.type === 'childList' && mutationType.toLowerCase() === 'childlist'){ return action(mutation);}
+            if(mutation.type === 'attributes' && mutationType.toLowerCase() === 'attributes'){ return action(mutation);}
+            if(mutation.type === 'childList' && mutationType.toLowerCase() === 'childlist_subtree'){ return action(mutation);}
+            if(mutation.type === 'attributes' && mutationType.toLowerCase() === 'attributes_subtree'){ return action(mutation);}   
         })
     });
-    observer.observe(mutationElement, mutationConfig);
-
-    
+    observer.observe(mutationElement, mutationConfig);    
 }
 
 function yourFunction(mutation) {
     console.log(mutation.type + " returned the following:", mutation);
+
 }
 
 
@@ -87,18 +78,18 @@ function yourFunction(mutation) {
 
 setTimeout(function(){
 
-    // text node changed.
+    // text node changed (text node is a child of the h1, so subtree is needed here when listening on the parent of the h1).
     // let h1Elem = document.querySelector(".container h1");
     // h1Elem.innerHTML = "Title was changed";
 
-    // addedNode changed.
+    // addedNode changed. (span is a direct child that is added to the .container so childlist is needed here).
     // let containerElem = document.querySelector(".container");
     // let span = document.createElement("span");
     // span.classList.add("added-span");
     // containerElem.appendChild(span);
 
-    // attribute changed.
-    // document.querySelector(".container h1").classList.add("my-new-class");
+    // attribute changed. (attribute_subtree is needed here because the change happened to a child of the element we were listening to: .container)
+    document.querySelector(".container h1").classList.add("my-new-class");
 
 
 
