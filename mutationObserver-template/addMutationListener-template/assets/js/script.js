cd867511c2104b28f84c508b24784603f3ab8328
Mikkel <mikkelmbk@live.dk>
2021-08-22 15:27:29 +0200

    added comments indication where to put code you want executed | added further description to the nodeName and className console logging.

--- mutationObserver-template/addMutationListener-template/assets/js/script.js
+++ mutationObserver-template/addMutationListener-template/assets/js/script.js
@@ -1,15 +1,16 @@
 if ('MutationObserver' in window) {
 
     addMutationListener(".container", "childlist_subtree", {
-        awNodeName: "p".toUpperCase(),
-        awClassName: "added-p"
+        awNodeName: "h1",
+        awFullClassName: "added-p",
+        awContainsClassName: "another-title"
     });
     //comments
     {
         // Parameter 1: element (class or Id) you want to listen to selector.
         // Parameter 2: mutation you want to listen for - possible settings (childList OR attributes OR childList_subtree OR attributes_subtree).
-        // Parameter 3: strings containing the nodeName and the className of the added, removed, altered, or otherwise affected element which you want to act on.
-        // All parameters are required (Parameter 3 will have an object of 2 empty strings the first time you run this, further explanation follows in the in depths section).
+        // Parameter 3: strings containing the nodeName and the FullclassName or ContainsClassName of the added, removed, altered, or otherwise affected element which you want to act on.
+        // All parameters are required (Parameter 3 will have an object of 3 empty strings the first time you run this, further explanation follows in the in depths section).
 
         // Parameter 1 in depths: You must provide an element by it's unique class or Id.
         // A class is produced by prefixing the name with a dot (.)
@@ -21,7 +22,10 @@ if ('MutationObserver' in window) {
         // The childList_subtree mutation will listen for changes to the direct children of the element you provided in parameter 1, but it will also listen for changes on children of children.
         // The attributes mutation will listen for attribute changes on the element you provided in parameter 1, but it will also listen for changes on children of children.
 
-        // Parameter 3 in depths: An object with 2 string values used to narrow down the mutations that you are receiving to only the one you want to listen for. Initially both strings will be empty, and you will need to find the 2 required values in the developer tool's console. The values are presented to you like seen in this image: https://explain.helloretail.com/OAunqA0j. Sometimes you will encounter more than one nodeName and className like seen in this image: https://explain.helloretail.com/Wnu0Jj2G, as a rule of thumb the last one should be used unless you have a reason not to.
+        // Parameter 3 in depths: An object with 3 string values used to narrow down the mutations that you are receiving to only the one you want to listen for. Initially all 3 strings will be empty, and you will need to find the 2 required values in the developer tool's console. The values are presented to you like seen in this image: https://explain.helloretail.com/OAunqA0j. Sometimes you will encounter more than one nodeName and className like seen in this image: https://explain.helloretail.com/Wnu0Jj2G, as a rule of thumb the last one should be used unless you have a reason not to.
+
+        // When parameter 2 is childlist or childlist_subtree, only awNodeName and awFullClassName are required.
+        // When parameter 2 is attributes or attributes_subtree, only awNodeName and awContainsClassName are required, only one class is accepted in the awContainsClassName string: https://explain.helloretail.com/4gul9gD8.  
         
 
 
@@ -74,19 +78,18 @@ function addMutationListener(element, mutationType, awConfig) {
 
 function mutationHandler(mutation, awConfig) {
     if (mutation.type === 'attributes') {
-        console.log(mutation.target.nodeName + " | " + mutation.target.className);
+        console.log("nodeName: " +mutation.target.nodeName + " | " + "className: " + mutation.target.className);
     }
     else if (mutation.type === 'childList') {
-        console.log(mutation.addedNodes[0].nodeName + " | " + mutation.addedNodes[0].className);
+        console.log("nodeName: " + mutation.addedNodes[0].nodeName + " | " + "className: " +mutation.addedNodes[0].className);
     }
 
-    if (mutation.type === 'childList' && mutation.addedNodes[0].className === awConfig.awClassName && mutation.addedNodes[0].nodeName === awConfig.awNodeName) {
-        // console.log("Hello world");
-
+    if (mutation.type === 'childList' && mutation.addedNodes[0].className === awConfig.awClassName && mutation.addedNodes[0].nodeName === awConfig.awNodeName.toUpperCase()) {
+        // If parameter 2 is childlist or childlist_subtree, the stuff you want to do goes in this if statement.
     }
 
-    if (mutation.type === 'attributes' && mutation.target.classList.contains("my-new-class") && mutation.target.nodeName === "H1") {
-
+    if (mutation.type === 'attributes' && mutation.target.classList.contains(awConfig.awContainsClassName.split(" ")[0]) && mutation.target.nodeName === awConfig.awNodeName.toUpperCase()) {
+        // If parameter 2 is attributes or attributes_subtree, the stuff you want to do goes in this if statement.
     }
 
 }
@@ -103,28 +106,29 @@ setTimeout(function () {
 
     // addedNode changed. (span is a direct child that is added to the .container so childlist is needed here).
     let containerElem = document.querySelector(".container");
-    let span = document.createElement("span");
-    span.classList.add("added-span");
-    containerElem.appendChild(span);
+    // let span = document.createElement("span");
+    // span.classList.add("added-span");
+    // containerElem.appendChild(span);
 
     let p = document.createElement("p");
     p.classList.add("added-p");
     containerElem.appendChild(p);
 
-    let article = document.createElement("article");
-    article.classList.add("added-article");
-    containerElem.appendChild(article);
+    // let article = document.createElement("article");
+    // article.classList.add("added-article");
+    // containerElem.appendChild(article);
 
-    let header = document.createElement("header");
-    header.classList.add("added-header");
-    containerElem.appendChild(header);
+    // let header = document.createElement("header");
+    // header.classList.add("added-header");
+    // containerElem.appendChild(header);
 
     // let figure = document.createElement("figure");
     // figure.classList.add("added-figure");
     // containerElem.appendChild(figure);
 
     // attribute changed. (attribute_subtree is needed here because the change happened to a child of the element we were listening to: .container)
-    // document.querySelector(".container h1").classList.add("my-new-class");
+    document.querySelector(".container h1").classList.add("my-new-class");
+    document.querySelector(".container .h2").classList.add("my-new-class");
     // document.querySelector(".container h1").classList.add("my-new-class2");
 
 
